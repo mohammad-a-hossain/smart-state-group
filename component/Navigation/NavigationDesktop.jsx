c7e97a16894c8a4024ea858510a9879842829c4f
mohammad-a-hossain <adam.eve007@gmail.com>
2023-06-23 18:36:56 +0600

    setup chakra ui and swiper

---
+++ component/Navigation/NavigationDesktop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+export const NavigationDesktop = () => {
+  return (
+    <Box
+    color='blue.500'
+    paddingY='2rem'
+    backgroundColor='white'
+    display={{base:'none', md:'block'}}
+    >
+    test
+    </Box>
+  )
+}
---
+++ component/Navigation/NavigationMobile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NavigationMobile = () => {
+  return (
+    <div>NavigationMobile</div>
+  )
+}
